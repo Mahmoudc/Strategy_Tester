69adcc544f6490e19a00a92498dddd6280aeaae5
Mahmoud Chahine <mahmoud101998@yahoo.com>
2022-09-23 11:53:35 -0400

    Initial Commit

---
+++ strategy_simulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  strategy_simulation
+//
+//  Created by Mahmoud Chahine on 9/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
